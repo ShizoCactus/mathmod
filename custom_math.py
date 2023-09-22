153051c1b21525608fb9b43d3f886e609662cf80
iLia <61847421+ShizoCactus@users.noreply.github.com>
2023-09-22 21:31:00 +0300

    Update custom_math.py

--- custom_math.py
+++ custom_math.py
@@ -1,71 +1,146 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import custom_math
 
 
-def my_dot(a, b): # Скалярное произведение векторов
-    result = 0
-    for i in range(len(a)):
-        result += a[i] * b[i]
-
-    return result
+def f(a, b, c, x1, x2):
+    return x1 + x2  #(x2 - c) ** 2 * np.sin(a * x1 + b * x2)
 
 
-def my_dot2(a, b): # Умножение матрицы на вектор
-    result = [0] * len(a)
-
-    for i in range(len(a)):
-        for j in range(len(b)):
-            result[i] += a[i][j] * b[j]
-
-    return result
+def cartesian(arr1, arr2):
+    return np.array([np.array([x, y]) for x in arr1 for y in arr2])
 
 
-def my_transpose(matrix): # Транспонирование матрицы
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    transposed_matrix = np.zeros((cols, rows))
-
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-
-    return transposed_matrix
-
-
-def my_cholesky(A): # Разложение Холецкого
-    n = A.shape[0]
-    L = np.zeros((n, n))
-
+def transform_array_to_matrix(arr):
+    n = int(len(arr) ** 0.5)
+    matrix = np.zeros((n, n))
     for i in range(n):
-        for j in range(i + 1):
-            if i == j:
-                L[i][j] = np.sqrt(A[i][i] - np.sum(L[i][:j] ** 2))
-            else:
-                L[i][j] = (A[i][j] - my_dot(L[i][:j], L[j][:j])) / L[j][j]
-
-    return L
+        for j in range(n):
+            matrix[i][j] = arr[n * i + j]
+    return custom_math.my_transpose(matrix)
 
 
-def my_solve(A, b): # Решение СЛАУ
-    try:
-        n = A.shape[0]
-        x = np.zeros(n)
+def generate_graph_data(a, b, c, n=50, noise_variance=1e-6):
+    np.random.seed(666)
+    x1 = np.random.uniform(-3.0, 3.0, n)
+    x1.sort()
+    x2 = np.random.uniform(-3.0, 3.0, n)
+    x2.sort()
+    X1, X2 = np.meshgrid(x1, x2)
+    Y = f(a, b, c, X1, X2)
+    X_graph = cartesian(x1, x2)
+    return X1, X2, Y, X_graph
 
-        for i in range(n-1, -1, -1):
-            x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
-    except:
-        x = np.linalg.solve(A, b)
 
-    return x
+def generate_data(a, b, c, n=10, noise_variance=1e-6): #Генерация случайных стартовых данных
+    np.random.seed(666)
+    x1 = np.linspace(-3.0, 3.0, n)
+    x1.sort()
+    x2 = np.linspace(-3.0, 3.0, n)
+    x2.sort()
+    X = cartesian(x1, x2)
+    x1 = X[:, 0]
+    x2 = X[:, 1]
+    y = f(a, b, c, x1, x2)
+    return x1, x2, y
 
 
-def get_diagonal(matrix): # Выделение диагонали квадратной матрицы
-    rows = len(matrix)
-    cols = len(matrix[0])
+def exponential_cov(x, y, sigma, l2): #Функция ядра
+    sqdist = np.sum(x**2, 1).reshape(-1,1) + np.sum(y**2, 1) - 2*np.dot(x, y.T)
+    return sigma ** 2 * np.exp(-0.5 * (1/l2) * sqdist)
 
-    if rows != cols:
-        raise ValueError("Input matrix is not square")
 
-    diagonal = [matrix[i][i] for i in range(rows)]
+def predict(X, Xtest, sigma=1, l2=0.1, noise_var=1e-6): #Основной алгоритм
+    N, n = len(X), len(Xtest)
+    K = exponential_cov(X, X, sigma, l2)
+    L = custom_math.my_cholesky(K)
+    Lk = custom_math.my_solve(L, exponential_cov(X, Xtest, sigma, l2))
+    mu = custom_math.my_dot2(custom_math.my_transpose(Lk), custom_math.my_solve(L, y))
+    K_ = exponential_cov(Xtest, Xtest, sigma, l2)
+    sd = np.sqrt(custom_math.get_diagonal(K_) - np.sum(Lk**2, axis=0))
+    return (mu, sd)
 
-    return diagonal
+
+sigma = 3 # Гиперпараметры
+l2 = 0.2
+n = 100
+
+a = 2 #Параметры
+b = 3
+c = 1
+x1, x2, y = generate_data(a, b, c)
+X = np.column_stack((x1, x2))  # Объединение двух переменных в одну матрицу
+
+
+X1, X2, Y, X_graph = generate_graph_data(a, b, c, n)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_surface(X1, X2, Y)
+plt.show()
+
+
+mu, sd = predict(X, X_graph, sigma, l2)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_surface(X1, X2, transform_array_to_matrix(mu))
+plt.show()
+
+
+
+
+# # Визуализация данных 2d
+# summa = np.array([t[0] + t[1] for t in X])
+# summa_graph = np.array([t[0] + t[1] for t in X_graph])
+# y_min = np.array(mu - sd)
+# y_max = np.array(mu + sd)
+# plt.plot(x2, y, 'x')
+# plt.plot(x2_graph, mu, label='Mean')
+# plt.fill_between(x2_graph, y_min, y_max, alpha=0.3)
+# plt.xlabel('X')
+# plt.ylabel('y')
+# plt.legend()
+# plt.show()
+#
+#
+# # Визуализация данных 3d
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, projection='3d')
+# ax.plot_surface(transform_array_to_matrix(x1_graph[271:1000]), transform_array_to_matrix(x2_graph[271:1000]), transform_array_to_matrix(y_graph[271:1000]))
+# plt.show()
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, projection='3d')
+# ax.plot_surface(transform_array_to_matrix(x1_graph[100:1000]), transform_array_to_matrix(x2_graph[100:1000]), transform_array_to_matrix(mu[100:1000]))
+# plt.show()
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, projection='3d')
+# ax.plot_surface(transform_array_to_matrix(x1_graph[100:1000]), transform_array_to_matrix(x2_graph[100:1000]), transform_array_to_matrix(mu[100:1000] - sd[100:1000]))
+# plt.show()
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, projection='3d')
+# ax.plot_surface(transform_array_to_matrix(x1_graph[100:1000]), transform_array_to_matrix(x2_graph[100:1000]), transform_array_to_matrix(mu[100:1000] + sd[100:1000]))
+# plt.show()
+#
+#
+# def check_accuracy(left, right):
+#     ans = 0
+#     for i in range(left, right):
+#         x1_g = x1_graph[i]
+#         x2_g = x2_graph[i]
+#         y_g = y_graph[i]
+#         m = mu[i]
+#         s = sd[i]
+#         b = m - s <= y_g <= m + s
+#         if b:
+#             ans += 1
+#         # print(i)
+#         # print('x1=', x1_g, 'x2=', x2_g, 'f=', y_g, 'm=', m, 's=', s, b)
+#     print(ans / (right - left))
+#
+#
+# check_accuracy(0, 1000)
+# check_accuracy(250, 1000)
+# check_accuracy(300, 1000)
